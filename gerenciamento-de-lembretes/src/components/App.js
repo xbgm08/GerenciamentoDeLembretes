91ae4b6cddf5baeaf02339b9c378838613b97b72
Gabriel Martins <gabrielbraulino@gmail.com>
2024-04-16 17:42:48 -0300

    feat(p1-pdmn): cria componente principal e faz hello

---
+++ gerenciamento-de-lembretes/src/components/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className="container border mt-2" >
+            <div className="row justify-content-center">
+                <div className="col-sm-12 col-md-8">
+                    <div>Hello, lembretes</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
